7156004ff9f43b34dd31e1220bee46ff099cead6
kapiorr <kapiorr@gmail.com>
2018-04-06 16:54:18 +0200

    Update start.sh
    Update permissions to:
    /var/tmp
    /var/run

--- start.sh
+++ start.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+
 chown -R nobody /var/www
 chown -R nobody /var/dokuwiki-storage
 chown -R nobody /var/tmp
 chown -R nobody /var/run
 
-su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
 
+su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
 exec /usr/bin/supervisord -c /etc/supervisord.conf

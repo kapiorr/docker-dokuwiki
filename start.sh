7308110529c2bdc1e5953416397e1d141f1cae2f
kapiorr <kapiorr@gmail.com>
2018-04-06 13:36:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ set -e
 
 chown -R nobody /var/www
 chown -R nobody /var/dokuwiki-storage
-chown -R nobody /var/tmp
-chown -R nobody /var/run
 
 su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
 
